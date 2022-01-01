178155b6276c3ff6455b8b9bfe7555409b387075
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-02 02:32:21 +0800

    Added definition of contract

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,3 +1,5 @@
 const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
