33012ecc881b2fcb96677a912b2db3d2949430bc
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-02 02:11:58 +0800

    Added main()

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,3 +6,9 @@ async function main() {
     console.log('Contract deployed to address:', hello_world.address);
 }
 
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
