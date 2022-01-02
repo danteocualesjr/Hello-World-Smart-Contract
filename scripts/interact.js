d5a1d0d325267b8114d9cd86e06461e848ef80ea
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-02 09:01:55 +0800

    Reading the init message/smart contract data from the Ethereum blockchain

--- scripts/interact.js
+++ scripts/interact.js
@@ -12,3 +12,9 @@ const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
 
 // Contract
 const helloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+
+async function main() {
+    const message = await helloWorldContract.message();
+    console.log('The message is: ' + message);
+}
+main();
