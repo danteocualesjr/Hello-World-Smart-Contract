643983675384861227050c0a505ebad3b1dad520
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-02 11:41:58 +0800

    Reading the new message

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,7 +1,8 @@
 const API_KEY = process.env.API_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
-const { ethers } = require("ethers");
+// const { ethers } = require("ethers");
+
 const contract = require("../artifacts/contracts/HelloWorld.sol/HelloWorld.json");
 
 // Provider
@@ -20,7 +21,8 @@ async function main() {
     console.log('Updating the message...');
     const tx = await helloWorldContract.update('This is the new message.');
     await tx.wait();
+
+    const newMessage = await helloWorldContract.message();
+    console.log('The new message is: ' + newMessage);
 }
 main();
-
-
