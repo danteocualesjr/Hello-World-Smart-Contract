0782371c2232a6b0bbf32c3f6ddc5347ca7450d0
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-04 12:24:59 +0800

    Added Etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,7 @@ require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 
 const { API_URL, PRIVATE_KEY } = process.env;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.7.3",
@@ -18,4 +19,9 @@ module.exports = {
       accounts: [`0x${PRIVATE_KEY}`]
     }
   },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apikey: PPVFWP1ZYZQ58RCZGI41V49GIG7KNMJBKD
+  }
 };
